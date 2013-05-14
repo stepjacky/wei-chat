e1114885e1f9f298b58fe4979f95faaad0da8f88
beshop <findufriend@gmail.com>
2013-05-15 02:16:13 +0800

    add lottery

--- application/views/front/header.php
+++ application/views/front/header.php
@@ -7,5 +7,12 @@
     <link rel="stylesheet" href="/resources/jquery-mobile/jquery.mobile-1.3.1.css"   />
     <script src="/resources/jquery-1.9.1.js" type="text/javascript"></script>
     <script src="/resources/jquery-mobile/jquery.mobile-1.3.1.js"></script>
+    <style type="text/css">
+        body{
+            margin: 0;
+            padding: 0;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
