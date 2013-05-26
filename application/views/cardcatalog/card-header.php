a2b2e74a446a78f46a0bd6d3b12b75a6e10bbc66
beshop <findufriend@gmail.com>
2013-05-26 23:21:21 +0800

    fix coupon bugs

--- application/views/cardcatalog/card-header.php
+++ application/views/cardcatalog/card-header.php
@@ -4,14 +4,14 @@
         <div class='card_logo_wrap'>
             <img src='/resources/images/card/logo.png' class="card_logo">
         </div>
-        <div class="dblock"></div>
+
         <div class="m_name"><?=$card['m_name']?></div>
         <div class="u_code"><?=str_pad($card['code'],6,'0',STR_PAD_LEFT)?></div>
     </div>
     <div  class="anim_b">
         <p>&nbsp;</p>
         <div class="m_info"><?=$card['m_info']?></div>
-        <div class="dblock"></div>
+        <div class="dblock35"></div>
         <div class="list">电话:<?=$card['m_phone']?></div>
         <div class="list">地址:<?=$card['m_address']?></div>
     </div>
